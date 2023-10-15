e3b56c323fa3c8e1a6d901f079a37335fb78db3d
Michael Pingleton <mnpingleton@gmail.com>
2023-10-15 00:40:11 -0400

    Moved compilation of source groups in the main file.

--- src/autobuild.cpp
+++ src/autobuild.cpp
@@ -10,6 +10,22 @@ int main(int argc, char** argv)
     AutobuildRepo repo;
     int result = repo.readAutobuildFile();
     
+    for (int i = 0; i < repo.sourceGroups.size(); i++)
+    {
+        cout << "Compiling source group " << repo.sourceGroups[i].id << "..." << endl;
+
+        for (int s = 0; s < repo.sourceGroups[i].compileJobs.size(); s++)
+        {
+            repo.sourceGroups[i].compileJobs[s].initObjectFile();
+
+            cout << "\t" << repo.sourceGroups[i].compileJobs[s].source.filename << " -> " << repo.sourceGroups[i].compileJobs[s].object.filename << "... ";
+            repo.sourceGroups[i].compileJobs[s].execute();
+            cout << "done!" << endl;
+        }
+
+        cout << endl;
+    }
+
     for (int i = 0; i < repo.targets.size(); i++)
     {
         cout << "Target #" << i << endl;
@@ -25,22 +41,6 @@ int main(int argc, char** argv)
         for (int s = 0; s < repo.targets[i].sourceGroupIds.size(); s++)
         {
             cout << "\tSource group: " << repo.targets[i].sourceGroupIds[s] << endl;
-        }
-
-        cout << endl;
-    }
-
-    for (int i = 0; i < repo.sourceGroups.size(); i++)
-    {
-        cout << "Compiling source group " << repo.sourceGroups[i].id << "..." << endl;
-
-        for (int s = 0; s < repo.sourceGroups[i].compileJobs.size(); s++)
-        {
-            repo.sourceGroups[i].compileJobs[s].initObjectFile();
-
-            cout << "\t" << repo.sourceGroups[i].compileJobs[s].source.filename << " -> " << repo.sourceGroups[i].compileJobs[s].object.filename << "... ";
-            repo.sourceGroups[i].compileJobs[s].execute();
-            cout << "done!" << endl;
         }
 
         cout << endl;
